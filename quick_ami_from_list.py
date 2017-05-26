3c4da7ddad8d9cff52bb1ff14d173abae0036086
Coin Graham <cgraham@2ndwatch.com>
2017-05-26 12:30:56 -0400

    updates to code to use additional tags and better return messaging.

--- quick_ami_from_list.py
+++ quick_ami_from_list.py
@@ -10,33 +10,49 @@ profile = quick_ami_from_list_config.profile
 no_reboot = quick_ami_from_list_config.no_reboot
 instance_list = quick_ami_from_list_config.instances
 description = quick_ami_from_list_config.description
+tag_name = quick_ami_from_list_config.tag
 time_string = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-try:
-    aws = boto3.Session(profile_name=profile, region_name=region)
-    ec2_client = aws.client('ec2')
-    ec2_resource = aws.resource('ec2')
-except:
-    raise
 
+def quick_ami_from_list():
 
-for instance in instance_list:
-    ami_name = instance + "_" + time_string
-    name_filter = [{
-        'Name': 'tag:Name',
-        'Values': [instance]
-    }]
     try:
-        res_collection = ec2_resource.instances.filter(Filters=name_filter)
-        for res in res_collection:
-            instance_id = res.id
-        print "Found instance {} with id {}\n".format(instance, instance_id)
-        response = ec2_client.create_image(
-            InstanceId=instance_id,
-            Name=ami_name,
-            Description=description,
-            NoReboot=no_reboot
-        )
-        print "Created image for {} with id {}\n".format(instance, response['ImageId'])
+        aws = boto3.Session(profile_name=profile, region_name=region)
+        ec2_client = aws.client('ec2')
+        ec2_resource = aws.resource('ec2')
     except:
         raise
+
+
+    for instance in instance_list:
+        instance_id = None
+        ami_name = instance + "_" + time_string
+        tag_name_filter = "tag:{}".format(tag_name)
+        name_filter = [{
+            "Name": tag_name_filter,
+            "Values": [instance]
+        }]
+        try:
+            res_collection = ec2_resource.instances.filter(Filters=name_filter)
+            for res in res_collection:
+                instance_id = res.id
+
+            if not instance_id:
+                return "No instances found.  Do you have the right profile, region, and tag?"
+
+            print "Found instance {} with id {}".format(instance, instance_id)
+            response = ec2_client.create_image(
+                InstanceId=instance_id,
+                Name=ami_name,
+                Description=description,
+                NoReboot=no_reboot
+            )
+            print "Created image for {} with id {}\n".format(instance, response['ImageId'])
+        except:
+            raise
+
+        return "AMI creation complete."
+
+if __name__ == "__main__":
+
+    print quick_ami_from_list()
--- quick_ami_from_list_config.py
+++ quick_ami_from_list_config.py
@@ -6,14 +6,17 @@ no_reboot = True  # This will cause instance to NOT reboot.
 
 # Profile you are referencing for authentication
 # profile = "firstwatch"  # for local testing
-profile = "CCNA_BI_NONPROD_Admin"
+profile = "008733_USEECTest_MSP"
 
 # Region you want to run in.
 # region = "us-east-1"  # for local testing
-region = "us-west-2"
+region = "us-east-1"
 
 # Description that you want for each AMI
 description = "Backup for migration image"
 
+# Tag to use for identification
+tag = "host_name"
+
 # Comma delimited array of quoted instance names to take AMI images.
-instances = ["CWDJDEA5001","CWDJDEA5011","CWDJDEA5012","CWDJDEA5013","CWDJDEA5014","CWDJDEA5015"]
+instances = ["cwdhaxa0001","cwdhaxd0001","cwdhaxw0001","cwdhaxw0002"]
